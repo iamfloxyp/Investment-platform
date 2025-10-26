3bfa8a39d7c716d6f732e6618f192389d8282bab
iamfloxyp <onye163@outlook.com>
2025-10-26 06:19:35 -0700

    Fix:Change to my vercel

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ const allowedOrigins = [
   "http://127.0.0.1:5501",
   "http://localhost:5500",
   "http://localhost:5501",
-  "https://investment-platform-dzz7.vercel.app", // ✅ your frontend
+  "https://emuntra-q35s.vercel.app",
 ];
 
 app.use(
