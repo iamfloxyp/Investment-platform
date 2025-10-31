7859abee159e82e10524c97eff15dec9e87ed5ad
iamfloxyp <onye163@outlook.com>
2025-10-31 11:55:17 -0400

    Fix:error in refferrallink

--- frontend/assets/js/user/referral.js
+++ frontend/assets/js/user/referral.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     // ✅ Update referral link dynamically
     const referralInput = document.getElementById("referralLink");
-    const referralUrl = `https://emuntra.vercel.app/user/index.html?ref=${referralCode}`;
+    const referralUrl = `https://investment-platform-eta.vercel.app/user/index.html?ref=${referralCode}`;
     // const referralUrl = `https://emuntra.com/?ref=${referralCode}`;
     referralInput.value = referralUrl;
 
